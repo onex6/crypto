44448ead2dd864aaf8580a49b61bf5a8ea184aa6
onex6 <dissjacques@gmail.com>
2019-01-16 11:22:12 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express= require('express');
 var app=express();
 const http = require('http');
-var router=require('./controllers/router');
 var logger= require('morgan');
 var bodyParser = require('body-parser');
 
